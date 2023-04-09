96cb56df1c80eb0d577d1689564e17dcf0fe0092
dcona943 <dcona1@rochester.rr.com>
2023-04-09 19:23:06 -0400

    Decision Tree

---
+++ CSC_Project.R
@@ -0,0 +1,57 @@
+#CSC Project
+pacman::p_load(datasets,pacman, dplyr, GGally, ggplot2, ggthemes, ggvis,
+               httr, lubridate, plotly, rio, rmarkdown, shiny,
+               stringr, tidyverse, lessR, aplpack, readr, tm, SnowballC, rpart.plot)
+test <- read.csv("D:/Software/R-4.2.2/RStudio/test.csv")
+train <- read.csv("D:/Software/R-4.2.2/RStudio/train.csv")
+#class and tweets
+#-----------------------------------------------------------------------------------------------------------------------------
+#Preprocessing train
+corpus <- Corpus(VectorSource(train$tweets))
+
+corpus <- tm_map(corpus,PlainTextDocument)
+corpus <- tm_map(corpus,tolower)
+corpus <- tm_map(corpus,removePunctuation)
+corpus <- tm_map(corpus,removeWords,stopwords("english"))
+corpus <- tm_map(corpus,stemDocument)
+
+freq <- DocumentTermMatrix(corpus)
+sparse <- removeSparseTerms(freq,.995)
+tSparse <- as.data.frame(as.matrix(sparse))
+colnames(tSparse) = make.names(colnames(tSparse))
+tSparse$class = train$class
+
+#Split the data
+trainSet <- create_train_test(tSparse,.8,train = T)
+testSet <- create_train_test(tSparse,.8,train = F)
+
+#Baseline 0.2608835  0.2566578  0.2284174  0.2540414
+prop.table(table(tSparse$class))
+#Make the Model
+fit <- rpart(class~., data = trainSet, method = "class")
+
+#Test model
+rpart.plot(fit)
+prediction <- predict(fit, testSet, type = "class")
+prediction
+
+table_mat <- table(testSet$class, prediction)
+accuracy_Test <- sum(diag(table_mat)) / sum(table_mat)
+accuracy_Test
+
+
+
+#Function
+create_train_test <- function(data, size = 0.8, train = TRUE) {
+  n_row = nrow(data)
+  total_row = size * n_row
+  train_sample <- 1: total_row
+  if (train == TRUE) {
+    return (data[train_sample, ])
+  } else {
+    return (data[-train_sample, ])
+  }
+}
+
+
+
