8e9805b3208feac75e1a57df798caca09eb3f2d2
CyberSakayia <126138303+CyberSakayia@users.noreply.github.com>
2023-04-19 01:18:38 -0400

    Update ForestAttempt3.R

--- ForestAttempt3.R
+++ ForestAttempt3.R
@@ -7,12 +7,16 @@ library(rpart)
 library(rpart.plot)
 library(rattle)
 library(partykit)
+library(reprtree)
+library(party)
 library(randomForestExplainer)
+install.packages("reprtree")
 train <- read.csv("D:/GitHub/R-Project/Datasets/Tweets_with_Sarcasm_and_Irony/train.csv")
 # https://cran.rstudio.com/web/packages/randomForestExplainer/vignettes/randomForestExplainer.html
 #class and tweets
 #-----------------------------------------------------------------------------------------------------------------------------
 #Preprocessing train
+
 corpus <- Corpus(VectorSource(train$tweets))
 
 corpus <- tm_map(corpus,PlainTextDocument)
@@ -31,25 +35,22 @@ tSparse$class = train$class
 trainSet <- create_train_test(tSparse,.8,train = T)
 testSet <- create_train_test(tSparse,.8,train = F)
 
-#RandomFores
+#RandomForest
 trainSet$class = factor(trainSet$class)
 testSet$class = factor(testSet$class)
 trainSet$class <- na.omit(trainSet$class)
 set.seed(120)
 classifier_RF <- randomForest(class~., trainSet, proximity=FALSE, ntree=150)
+print(classifier_RF)
 printRandomForests(classifier_RF, models=NULL, include.class=NULL, format="")
 
 #GUI model work in progress Dont let Eric use it
-forestModel <- ntree(classifier_RF)
-?randomForest
-explainer <- explain_rf(classifier_RF, data = trainSet)
 
-for (i in 1:150) {
-  tree <- getTree(classifier_RF, k=i, labelVar=TRUE)
-  party_tree <- as.party(tree)
-  cat("Tree", i, "\n")
-  plot(party_tree, type="simple", terminal_panel=node_terminal)
-}
+x <- ctree(class ~ ., data=trainSet, controls=cforest_control(mtry=2, mincriterion=0))
+plot(x, type="simple")
+forestModel <- ctree(classifier_RF)
+
+explainer <- explain_rf(classifier_RF, data = trainSet)
 
 # calculates the interactions_frame for default settings so may give different
 # results than the function below depending on our settings
@@ -61,7 +62,7 @@ head(min_depth_frame, n = 10)
 plot_min_depth_distribution(min_depth_frame)
 plot_min_depth_interactions(classifier_RF) # takes a long time to run
 
-#prediction and confusion matrix for train and test set
+#prediction and confusion matrix for train set
 p1 <- predict(classifier_RF, trainSet)
 confusionMatrix(p1, trainSet$class)
 
